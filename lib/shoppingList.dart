158882809a6f40b36e67d5ff858d02fb955503ed
Kedar Rudrawar <rudrawarkedar@gmail.com>
2019-11-23 16:35:56 -0800

    Created basic shopping list to show hardcoded elements

--- lib/shoppingList.dart
+++ lib/shoppingList.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
+
   @override
   Widget build(BuildContext buildContext){
     return MaterialApp(
@@ -20,12 +21,38 @@ class ShoppingList extends StatefulWidget{
 
 class ShoppingListState extends State<ShoppingList>{
   List<String> ingredients;
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext buildContext){
     setState(() {
       ingredients = ['a', 'b', 'c'];
     });
-    return ListView();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping List"),
+      ),
+      body: _createIngredientList(),
+    );
+  }
+
+
+  Widget _createIngredientList(){
+    return ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: ingredients.length,
+        itemBuilder: (context, i) {
+          return _buildRow(ingredients[i]);
+        }
+    );
+  }
+
+  Widget _buildRow(String ingredient){
+    return ListTile(
+      title: Text(ingredient,
+        style: _biggerFont,
+      )
+    );
   }
 }
