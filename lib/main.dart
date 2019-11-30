34ece0148280fb48a4af96603fb29c15a2726580
MazyarM6 <mmirzaei@ucsd.edu>
2019-11-29 21:29:18 -0800

    screenSize solved

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:cookmate/homePage.dart';
+import 'package:cookmate/util/backendRequest.dart';
 import 'package:flutter/material.dart';
+import 'cookbook.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  BackendRequest request =
+      BackendRequest("03740945581ed4d2c3b25a62e7b9064cd62971a4", 2);
+  List<Recipe> recipes;
+
+  void makeRequest() async {
+    request.getPopularRecipes().then((popularRecipes) {
+      recipes = popularRecipes;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    makeRequest();
     return MaterialApp(
+
       title: 'CookMate',
       theme: ThemeData(),
       home: MyHomePage(),
